658e8c5a13b38d428af647562c4e20f480c2674b
inspikalu <inspikalu@gmail.com>
2024-03-25 20:26:04 +0100

    Updated the vision

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -5,7 +5,35 @@ export default function Vision() {
         <h1 className="text-center text-xl text-[var(--color-main)] font-extrabold">
           Our Vision
         </h1>
-        <p className="block text-center">Our vision is to </p>
+        {/* <p className="block text-center">Our vision is to </p> */}
+        <ul className="list-disc flex justify-center items-center flex-col gap-4">
+          <li>
+            Offering a place of safety for children, women and girls fleeing
+            domestic violence, rape and sexual exploitation
+          </li>
+          <li>
+            Collaborating with local government to undertake and publish
+            thematic assessments on issues like domestic violence, rape and
+            sexual exploitation, child labour, forced marriages, poverty and
+            child protection and discrimination
+          </li>
+          <li>
+            Offering advocacy, counselling, psychological and therapeutic
+            support services for the vulnerable especially women, girls and
+            children who are victims of abuse, and marginalisation
+          </li>
+          <li>
+            Offering support, skill acquisition, training, and rehabilitation
+            for victims of gender based violence, forced labour, sexual
+            exploitation
+          </li>
+          <li>
+            Psychological, wellbeing and counselling services for people dealing
+            with stressful and depressing life changing circumstance for example
+            infertility, miscarriages, marital breakdown, unemployment, poverty
+            separation and loss, natural disasters and addictions
+          </li>
+        </ul>
       </div>
     </>
   );
